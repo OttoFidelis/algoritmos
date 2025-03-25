af05f4c6d1f23633187affbca5d740f84beece62
unknown <ottofidelis@gmail.com>
2025-03-25 09:29:40 -0300

    coisei os coiso

--- projeto1/src/App.java
+++ projeto1/src/App.java
@@ -1,15 +1,48 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        int num = 5;
+    public static void main(String[] args) throws IOException {
+        Scanner tcd = new Scanner(System.in);
+        int num = tcd.nextInt();
         String bin = "";
-        while(num !=1){
-            bin +=num%2;
-            num/=2;
-            
+        while (num > 1) {
+            int resto = num % 16;
+            switch (resto) {
+                case 10:
+                    bin += "A";
+                    break;
+
+                case 11:
+                    bin += "B";
+                    break;
+
+                case 12:
+                    bin += "C";
+                    break;
+
+                case 13:
+                    bin += "D";
+                    break;
+
+                case 14:
+                    bin += "E";
+                    break;
+
+                case 15:
+                    bin += "F";
+                    break;
+
+                default:
+                    bin += resto;
+                    break;
+            }
+            num /= 16;
         }
-        bin+=num;
+        if (num != 0)
+            bin += num;
         String binReverse = new StringBuilder(bin).reverse().toString();
-        System.out.println(Long.parseLong(binReverse));
+        System.out.println("Versão hexadecimal do número: "+binReverse);
 
     }
 }
